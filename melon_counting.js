8f2d5235280df74ab44aef8e71ca5936ca425168
Alejandra Lopez <alemarlo1992@gmail.com>
2019-10-24 21:14:02 -0700

    Updated melon_counting.js

---
+++ melon_counting.js
@@ -0,0 +1,27 @@
+let melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+const countMelons = (melonList) => {
+    const melonCounts = {}
+    for (let melon of melonList) {
+        if (melonsToAdd[melon]) {
+            melonCounts[melon] += 1;
+        }else {
+            melonCounts[melon] = 1 ;
+        }
+
+    }
+
+    console.log(melonCounts);
+}
+
+countMelons(['Watermelon', 'Santa Claus', 'Casaba', 'Horned Melon', 'Xigua'])
